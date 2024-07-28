0ce802b092ca847e026790b1c970762bd521b430
Sahil <omanwarsahil@gmail.com>
2024-07-28 14:22:06 +0530

    Routes Changed

--- server/routes/bookRoute.js
+++ server/routes/bookRoute.js
@@ -1,16 +1,18 @@
-const express=require('express');
-const router=express.Router();
+ const express=require('express');
+ const router=express.Router();
 
 
-const {  createBook, deleteBook,getBook, updateBook, getAllBooks} =require('../controller/bookController');
+ const {  createBook, deleteBook,getBook, updateBook, getAllBooks,searchBooksByName} =require('../controller/bookController');
 
 
-router.post('/createBook',createBook);
+ router.post('/createBook',createBook);
 router.get('/getBook/:id', getBook);
-router.put('/updateBook/:id',updateBook)
-router.delete('/deleteBook/:id',deleteBook);
-router.get('/all-books',getAllBooks)
+ router.put('/updateBook/:id',updateBook)
+ router.delete('/deleteBook/:id',deleteBook);
+router.get('/all-books',getAllBooks);
+ router.get('/search/:name', searchBooksByName);
 
 
 
 module.exports=router;
+
