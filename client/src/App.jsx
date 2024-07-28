8e311953b6e66f839b27de7472f2c09f3608e14d
Sahil <omanwarsahil@gmail.com>
2024-07-28 14:26:10 +0530

    New Routes defined

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,8 @@ import Blog from './components/Blog';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
 import SingleBook from './pages/SingleBook';
+import Sell from './pages/Sell.jsx';
+import SearchByName from './components/SearchByName.jsx';
 
 function App() {
   return (
@@ -18,6 +20,11 @@ function App() {
         <Route path="/about" element={<About />} />
         <Route path="/blog" element={<Blog />} />
         <Route path="/book/:id" element={<SingleBook />} />
+        <Route path='/admin/dashboard' element={<Sell></Sell>}></Route>
+        <Route path="/book/search/:name" element={<SearchByName />} />
+        <Route path="/book/search/:name" element={<SearchByName />} />
+
+
       </Routes>
       <Footer />
     </BrowserRouter>
@@ -25,3 +32,4 @@ function App() {
 }
 
 export default App;
+
